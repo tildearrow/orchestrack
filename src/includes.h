6ed2ef8d980a61343de8452843b6f5d6c8434369
tildearrow <tildearrow@users.noreply.github.com>
2016-08-14 23:01:52 -0500

    includes.h: add stdarg, vector, string and queue

--- src/includes.h
+++ src/includes.h
@@ -1,6 +1,10 @@
 #ifndef INCLUDES_H
 #define INCLUDES_H
 #include <stdio.h>
+#include <stdarg.h>
 #include <SDL2/SDL.h>
 #include <json.hpp>
+#include <vector>
+#include <string>
+#include <queue>
 #endif
