6130aca9c7d2fc1840d34d203c6935aa62162592
tildearrow <tildearrow@users.noreply.github.com>
2016-08-16 22:37:50 -0500

    change instr.h
    i think it'll end up being plugin rather than instrument...

--- src/instr/instr.h
+++ src/instr/instr.h
@@ -1,13 +1,26 @@
 #include "../../includes.h"
 
+// Instrument specification.
+struct OTrackInsSpec {
+  const char* name;
+  const char* desc;
+  const char* author;
+  const char* copyright;
+  const char* license;
+  int inChannels, outChannels;
+  bool midiIn, midiOut;
+}
+
 class OTrackInstrument {
   SDL_Renderer* r;
+  float* sample;
   std::queue<void*> event;
   public:
     std::vector<float> param;
     void submitEvent(void* data);
     void* getEvent();
-    virtual void getSample(int channels, float* sample, ...);
+    virtual void* getSample();
     virtual void drawUI();
     void setRenderer(SDL_Renderer* renderer);
+    void init(OTrackInsSpec* initSpec, OTrackInsSpec* resultSpec);
 };
