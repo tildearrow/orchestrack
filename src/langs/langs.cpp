d41b0f93d7c1c4a7c1800fdc0256766e77993b28
tildearrow <tildearrow@users.noreply.github.com>
2016-09-22 14:37:13 -0500

    more work on language support

--- src/langs/langs.cpp
+++ src/langs/langs.cpp
@@ -9,6 +9,8 @@
 std::string* lsLoad(const char* dir, const char* lang) {
   FILE* f;
   char* fn;
+  std::string* ret;
+  int c, ci, s, t;
   if (dir==NULL || lang==NULL) {
     return 0;
   }
@@ -21,4 +23,29 @@ std::string* lsLoad(const char* dir, const char* lang) {
     return 0;
   }
   delete[] fn;
+  while (!feof(f)) {
+    if (fgetc(f)=='\n') {
+      t++;
+    }
+  }
+  fseek(f,0,SEEK_SET);
+  ret=new std::string[t];
+  s=0;
+  ci=0;
+  while (!feof(f)) {
+    c=fgetc(f);
+    switch (c) {
+      case '\t':
+        s=1;
+	break;
+      case '\n':
+        s=0; ci++;
+	break;
+      default:
+        if (s) ret[ci]+=c;
+        break;
+    }
+  }
+  fclose(f);
+  return ret;
 }
