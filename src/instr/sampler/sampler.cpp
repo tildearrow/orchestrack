e98782b33e275c8f93f4e8dea9bba42341b51eb5
tildearrow <tildearrow@users.noreply.github.com>
2016-10-10 15:56:49 -0500

    sampler: faster sinc interpolator

--- src/instr/sampler/sampler.cpp
+++ src/instr/sampler/sampler.cpp
@@ -53,16 +53,18 @@ inline float Sampler::intCubic(float* b, int n, float d) {
 }
 
 inline float Sampler::intSinc(float* b, int n, float d) {
-  float one;
-  one=0;
   int pl;
   float* where;
   pl=((int)(d*65536))&0xffff;
   where=table+(((pl+fHalve)>>fShift)&fMask);
-  for (int i=-3; i<5; i++) {
-    one+=b[n+i]*where[i+3];
-  }
-  return one;
+  return b[n-3]*where[0]+
+         b[n-3]*where[1]+
+         b[n-1]*where[2]+
+         b[ n ]*where[3]+
+         b[n+1]*where[4]+
+         b[n+2]*where[5]+
+         b[n+3]*where[6]+
+         b[n+4]*where[7];
 }
 
 float* Sampler::getSample() {
