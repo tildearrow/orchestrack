81f17831705b34032760b4a9fa976d42099063d5
tildearrow <tildearrow@users.noreply.github.com>
2016-11-08 00:28:19 -0500

    sampler (state): partially implement saving

--- src/instr/sampler/state.cpp
+++ src/instr/sampler/state.cpp
@@ -10,5 +10,123 @@ bool Sampler::loadState(FILE* data) {
 }
 
 unsigned char* Sampler::saveState(int* size) {
-  return NULL;
+  FILE* fi;
+  if (!(fi=tmpfile())) {
+    return NULL;
+  }
+  // header chunk //
+  fputc('h',fi);
+  fputc('e',fi);
+  fputc('a',fi);
+  fputc('d',fi);
+  // see doc/instr/dev/format.md
+  fputc(0,fi); // audio in
+  fputc(2,fi); // audio out (for now)
+  fputc(1,fi); // midi in
+  fputc(0,fi); // midi out
+  fputs("OTRKSMP",fi); // identifier
+  fputs("Sampler",fi); // name (for now)
+  // sample header //
+  // see doc/instr/sampler/tech.md
+  fputc('s',fi);
+  fputc('m',fi);
+  fputc('p',fi);
+  fputc('h',fi);
+  fputc(36,fi); // the header is exactly 36 bytes
+  fputc(0,fi);
+  fputc(0,fi);
+  fputc(0,fi);
+  fputi(1,fi); // version 1
+  fputs("\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0",fi); // padding for now...
+  fputi(sSize,fi); // sample count
+  fputi(eSize,fi); // env count
+  fputi(0,fi); // coming soon?
+  // samples //
+  for (int i=0; i<sSize; i++) {
+    fputc('s',fi);
+    fputc('m',fi);
+    fputc('p',fi);
+    fputc('s',fi);
+    fputi(0,fi); // skip for now, size will be written later
+    fputi(i,fi); // number
+    fputs(s[i].path->c_str(),fi); // name
+    fputf(s[i].rate,fi); // rate
+    fputi(0,fi); // padding
+    fputc(s[i].noteMin,fi);
+    fputc(s[i].noteMax,fi);
+    fputc(s[i].velMin,fi);
+    fputc(s[i].velMax,fi);
+    fputi(0,fi); // padding, because i may
+    fputi(0,fi); // add something later
+    fputi(0,fi); 
+    // volume env
+    fputi(s[i].envVol,fi);
+    fputf(s[i].volAmt,fi);
+    fputf(s[i].volCap,fi);
+    fputi(0,fi);
+    // panning env
+    fputi(s[i].envPan,fi);
+    fputf(s[i].panAmt,fi);
+    fputf(s[i].panCap,fi);
+    fputi(0,fi);
+    // pitch env
+    fputi(s[i].envPitch,fi);
+    fputf(s[i].pitchAmt,fi);
+    fputf(s[i].pitchCap,fi);
+    fputi(0,fi);
+    // cutoff env
+    fputi(s[i].envCut,fi);
+    fputf(s[i].cutAmt,fi);
+    fputf(s[i].cutCap,fi);
+    fputi(0,fi);
+    // resonance env
+    fputi(s[i].envRes,fi);
+    fputf(s[i].resAmt,fi);
+    fputf(s[i].resCap,fi);
+    fputi(0,fi);
+    // filter, and others
+    fputc(s[i].filter,fi);
+    fputc(0,fi); // more padding
+    fputc(0,fi);
+    fputc(0,fi);
+    fputi(s[i].loopStart,fi);
+    fputi(s[i].loopEnd,fi);
+    fputi(s[i].loopType,fi);
+    fputi(s[i].len,fi);
+    fputi(s[i].chan,fi);
+    // TODO:
+    // - flags
+    // - remote flag
+    fputi(0,fi);
+    // sample data
+    for (int j=0; j<(s[i].len*s[i].chan); j++) {
+      fputf(s[i].data[j%s[i].chan][j/s[i].chan],fi);
+    }
+  }
+  // envelopes //
+  for (int i=0; i<eSize; i++) {
+    fputc('s',fi);
+    fputc('m',fi);
+    fputc('p',fi);
+    fputc('e',fi);
+    fputi(0,fi); // skip for now, size will be written later
+    fputi(i,fi); // number
+    fputs(e[i].name->c_str(),fi); // name
+    fputi(e[i].start,fi);
+    fputi(e[i].susStart,fi);
+    fputi(e[i].susEnd,fi);
+    fputi(e[i].loopStart,fi);
+    fputi(e[i].loopEnd,fi);
+    fputi(65536,fi); // precision
+    fputi(e[i].start,fi); // TODO: flags
+    fputi(e[i].pSize,fi); // point count
+    // points
+    for (int j=0; j<e[i].pSize; j++) {
+      fputi(e[i].p[j].time,fi);
+      fputf(e[i].p[j].value,fi);
+      fputi(0,fi);
+      fputf(0.0f,fi);
+    }
+  }
+  // TODO: sizes, and actually saving. good night.
 }
